3ebed49bca89630a327426f33c245a1f0fbbc64a
Martin <m.moolgaard@gmail.com>
2020-10-20 13:19:40 +0200

    initial commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+c="13"
+d=input("type 37 and press [enter]:")
+e=37
+
+cc=int(c)
+dd=int(d)
+
+print(c+d)
+print(cc+dd)
+print(cc+e)
+print(c+e)
+
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(cc))
